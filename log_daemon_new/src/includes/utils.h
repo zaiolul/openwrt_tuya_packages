dce5999a3886ce1f15f72af962e56d4e29f4ee63
zaiolul <zaiowrk321@gmail.com>
2023-04-03 10:43:34 +0300

    more refactor

--- log_daemon_new/src/includes/utils.h
+++ log_daemon_new/src/includes/utils.h
@@ -48,75 +48,14 @@ struct esp_response{
     char message[1024];
 };
 
-enum {
-    UPTIME_VALUE,
-    UPTIME_MAX
-};
-enum{
-    DEVICE_NAME,
-    DEVICE_PORT,
-    DEVICE_VID,
-    DEVICE_PID,
-    DEVICE_MAX
-};
-enum {
-    DEVICES_INFO_ARR,
-    DEVICES_INFO_MAX
-};
-
-enum {
-    CONTROL_RESPONSE_VALUE,
-    CONTROL_MESSAGE,
-    CONTROL_MAX
-};
-
-
-static const struct blobmsg_policy info_policy[UPTIME_MAX] = {
-	[UPTIME_VALUE] = { .name = "uptime", .type = BLOBMSG_TYPE_INT32},
-};
-static const struct blobmsg_policy device_data_policy[DEVICE_MAX] = {
-    [DEVICE_NAME] = { .name = "esp_name", .type = BLOBMSG_TYPE_STRING},
-	[DEVICE_PORT] = { .name = "port", .type = BLOBMSG_TYPE_STRING},
-    [DEVICE_PID] = { .name = "pid", .type = BLOBMSG_TYPE_STRING},
-    [DEVICE_VID] = { .name = "vid", .type = BLOBMSG_TYPE_STRING},
-};
-
-static const struct blobmsg_policy devices_policy[DEVICES_INFO_MAX] = {
-	[DEVICES_INFO_ARR] = { .name = "devices", .type = BLOBMSG_TYPE_ARRAY},
-};
-
-static const struct blobmsg_policy control_policy[CONTROL_MAX] = {
-	[CONTROL_RESPONSE_VALUE] = { .name = "response", .type = BLOBMSG_TYPE_INT32},
-    [CONTROL_MESSAGE] = {.name = "msg", .type = BLOBMSG_TYPE_STRING}
-};
-
-
 /*make program a daemon*/
 int daemonize();
 /*gets value field from json MESSAGE and writes to FILENAME upon receiving PARAMETER set*/
 int write_to_file(char* parameter, char* message, char* filename);
-/*uptime callback function*/
-void uptime_cb(struct ubus_request *req, int type, struct blob_attr *msg);
-/*device callback function*/
-void devices_cb(struct ubus_request *req, int type, struct blob_attr *msg);
-/*gpio state callback function*/
-void state_cb(struct ubus_request *req, int type, struct blob_attr *msg);
-/*start ubus context*/
-int ubus_start(struct ubus_context **ctx, char *path, uint32_t *id);
-/*check invoke method return value*/
-void check_ubus_return_value(int value);
 /*main program loop*/
 int main_func(struct arguments arguments);
 /*program signal handler*/
 void sig_handler(int signum);
-/*sends data about connected ESP devices to cloud*/
-int report_device_data();
-/*sends device uptime data to cloud*/
-int report_uptime_data();
-/*changes the state of gpio pin depending on the input from cloud, sends back response*/
-int change_gpio_state(cJSON *input);
-/*invokes ubus method*/
-int invoke_method(struct ubus_context *ctx, uint32_t id, char *method, struct blob_attr *msg,
-    ubus_data_handler_t cb, void *data);
+/*uptime callback function*/
 
 #endif
